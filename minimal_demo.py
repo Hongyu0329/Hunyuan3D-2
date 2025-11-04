222eccff22b52103c3d37e7fafa2885ddb32dfed
Hongyu0329 <htwanghongyu@163.com>
2025-11-04 19:54:48 +0000

    update new generation

--- minimal_demo.py
+++ minimal_demo.py
@@ -22,7 +22,7 @@ model_path = 'tencent/Hunyuan3D-2'
 pipeline_shapegen = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
 pipeline_texgen = Hunyuan3DPaintPipeline.from_pretrained(model_path)
 
-image_path = 'assets/demo.png'
+image_path = 'assets/cat.png'
 image = Image.open(image_path).convert("RGBA")
 if image.mode == 'RGB':
     rembg = BackgroundRemover()
@@ -30,4 +30,4 @@ if image.mode == 'RGB':
 
 mesh = pipeline_shapegen(image=image)[0]
 mesh = pipeline_texgen(mesh, image=image)
-mesh.export('demo.glb')
+mesh.export('cat.obj')
